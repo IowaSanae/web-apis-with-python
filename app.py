cd380a99724ee087c33eecccfc7489b6f32ba98f
Đỗ Gia Bảo <62286955+dobao1309@users.noreply.gitlab.com>
2023-02-09 16:58:07 +0700

    Updated app.py and dbHandler.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    # response = {"usage": "/dict?=<word>"}
+    response = {"usage": "/dict?=<word>"}
     # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+    return jsonify(response)
 
 
 @app.get("/dict")
@@ -26,7 +26,30 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+    words = request.args.get("word")
+
+    # Return an error querystring is malformed
+    if not words:
+        response = {"status": "error", "word": words, "data": "Word not found"}
+        return jsonify(response)
+
+    # Initialize the response
+    response = {"words": []}
+
+    for word in words:
+        # Try to find an exact match
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({"status": "success", "word": word, "data": definitions})
+        else:
+            # Try to find an approximate match
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status": "partial", "word": word, "data": definitions})
+            else:
+                response["words"].append({"status": "error", "word": word, "data": "Word not found"})
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
